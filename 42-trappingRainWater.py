dc85f81dc25f148ba88f1ece953a5b5116df2d9a
Alper Torun <alper.torun@piworks.net>
2024-06-21 10:45:32 +0300

    day10

---
+++ 42-trappingRainWater.py
@@ -0,0 +1,22 @@
+def trap(height):
+    if not height:
+        return 0
+
+    left, right = 0, len(height) - 1
+    left_max, right_max = height[left], height[right]
+    water = 0
+
+    while left < right:
+        if height[left] < height[right]:
+            left += 1
+            left_max = max(left_max, height[left])
+            water += left_max - height[left]
+        else:
+            right -= 1
+            right_max = max(right_max, height[right])
+            water += right_max - height[right]
+
+    return water
+
+height = [4,2,0,3,2,5]
+print(trap(height))
