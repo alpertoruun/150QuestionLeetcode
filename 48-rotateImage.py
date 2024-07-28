0a7cffffc1408859b4a0db4c9ecc09e35a384f7d
Alper Torun <alper.torun@piworks.net>
2024-07-28 16:10:42 +0300

    day19

---
+++ 48-rotateImage.py
@@ -0,0 +1,13 @@
+def rotate(matrix):
+    left,right=0,len(matrix[0])-1
+    while l < r:
+        for i in range(r-l):
+            top,bottom=l,r
+            topLeft=matrix[top][l+i]
+            matrix[top][l+i]=matrix[bottom-i][l]
+            matrix[bottom-i][l]=matrix[bottom][r-i]
+            matrix[bottom][r-i]=matrix[top+i][r]
+            matrix[top+i][r]topLeft
+        r-=1
+        l+=1
+    
